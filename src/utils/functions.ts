1f1e08dd9fa16e22b32e25da0856ae14133db6e5
vortexory <robothook001231@gmail.com>
2024-01-06 16:37:41 +0200

    formatPrice function

---
+++ src/utils/functions.ts
@@ -0,0 +1,22 @@
+const formatPrice = (price: number) => {
+  if (price >= 0.01) {
+    return price.toFixed(2);
+  } else {
+    const priceAsString = price.toString();
+
+    const decimals = priceAsString.split(".")[1];
+
+    let initialIndex = 0;
+
+    for (let i = 0; i <= decimals.length - 1; i++) {
+      if (decimals[i] === "0") {
+        initialIndex += 1;
+      } else {
+        break;
+      }
+    }
+
+    const toFixed = initialIndex + 2;
+    return price.toFixed(toFixed);
+  }
+};
