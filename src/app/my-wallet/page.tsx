8cb23c84cdd51680394f2f93db29457aecb1cb30
vortexory <robothook001231@gmail.com>
2024-01-18 23:12:27 +0200

    delete wallet UI

--- src/app/my-wallet/page.tsx
+++ src/app/my-wallet/page.tsx
@@ -28,6 +28,7 @@ import axios from "axios";
 import { useState } from "react";
 import { calculateAvgBuyPrice, formatPrice } from "@/utils/functions";
 import { useToast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
   const [selectedWallet, setSelectedWallet] = useState<UserWallet | null>(null);
@@ -80,6 +81,27 @@ const page = () => {
     },
   });
 
+  const { mutateAsync: deleteWalletAsync } = useMutation({
+    mutationFn: (wallet: { userId: string; walletId: string }) => {
+      return axios.patch("/api/wallet/delete-wallet", wallet);
+    },
+    onSuccess: () => {
+      setSelectedWallet(null);
+      queryClient.invalidateQueries({ queryKey: ["wallets"] });
+      toast({
+        title: "Wallet Deleted",
+        description: "The wallet has been deleted.",
+      });
+    },
+    onError: () => {
+      toast({
+        variant: "destructive",
+        title: "Something went wrong",
+        description: "There was a problem with your request.",
+      });
+    },
+  });
+
   const handleDeleteCoin = async (walletId: string, coinApiID: number) => {
     try {
       if (session?.user?.id) {
@@ -93,6 +115,21 @@ const page = () => {
       }
     } catch (error) {
       console.error("Error deleting coin", error);
+    }
+  };
+
+  const handleDeleteWallet = async () => {
+    try {
+      if (session?.user?.id && selectedWallet) {
+        const walletData = {
+          userId: session.user.id,
+          walletId: selectedWallet._id,
+        };
+
+        await deleteWalletAsync(walletData);
+      }
+    } catch (error) {
+      console.error("Error deleting wallet", error);
     }
   };
 
@@ -156,6 +193,15 @@ const page = () => {
             <Progress value={50} />
           </div>
         </div>
+        {selectedWallet && (
+          <Button
+            className="w-fit"
+            variant="destructive"
+            onClick={handleDeleteWallet}
+          >
+            Delete wallet
+          </Button>
+        )}
 
         <div className="flex gap-6">
           <Card>
