bf3f9de2aeb32fdad9029f29282ac85d6eb47c7f
vortexory <robothook001231@gmail.com>
2024-01-23 22:35:02 +0200

    delete wallet dialog

---
+++ src/components/DeletePortfolioDialog.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import React, { Dispatch, SetStateAction, useState } from "react";
+import { Button } from "./ui/button";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import { Session, UserWallet } from "@/utils/interfaces";
+import axios from "axios";
+import { useToast } from "./ui/use-toast";
+
+const DeletePortfolioDialog = ({
+  selectedWallet,
+  setSelectedWallet,
+}: {
+  selectedWallet: UserWallet | null;
+  setSelectedWallet: Dispatch<SetStateAction<UserWallet | null>>;
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { mutateAsync: deleteWalletAsync, isPending } = useMutation({
+    mutationFn: (wallet: { userId: string; walletId: string }) => {
+      return axios.patch("/api/wallet/delete-wallet", wallet);
+    },
+    onSuccess: () => {
+      setSelectedWallet(null);
+      queryClient.invalidateQueries({ queryKey: ["wallets"] });
+      toast({
+        title: "Wallet Deleted",
+        description: "The wallet has been deleted.",
+      });
+    },
+    onError: () => {
+      toast({
+        variant: "destructive",
+        title: "Something went wrong",
+        description: "There was a problem with your request.",
+      });
+    },
+  });
+
+  const queryClient = useQueryClient();
+
+  const { toast } = useToast();
+
+  const { data } = useSession();
+
+  const session = data as Session | null;
+
+  const handleDeleteWallet = async () => {
+    try {
+      if (session?.user?.id && selectedWallet) {
+        const walletData = {
+          userId: session.user.id,
+          walletId: selectedWallet._id,
+        };
+
+        await deleteWalletAsync(walletData);
+      }
+    } catch (error) {
+      console.error("Error deleting wallet", error);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <Button
+        className="w-fit"
+        variant="destructive"
+        onClick={() => setIsModalOpen(true)}
+      >
+        Delete wallet
+      </Button>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>
+            Are you sure you want to delete {selectedWallet?.name}?
+          </DialogTitle>
+        </DialogHeader>
+
+        <DialogFooter className="mt-3">
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={() => setIsModalOpen(false)}
+            className="flex-1"
+          >
+            Close
+          </Button>
+          <Button
+            type="button"
+            variant="destructive"
+            onClick={handleDeleteWallet}
+            className="flex-1"
+            disabled={isPending}
+          >
+            Delete
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DeletePortfolioDialog;
