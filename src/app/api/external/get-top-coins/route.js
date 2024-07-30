f22cadc0d8506b8b64cc71eee4ef004acf693b34
vortexory <robothook001231@gmail.com>
2024-07-30 23:23:08 +0300

    add cache control no-store

--- src/app/api/external/get-top-coins/route.js
+++ src/app/api/external/get-top-coins/route.js
@@ -8,13 +8,14 @@ export const GET = async () => {
       return new Response("API Key not found", { status: 401 });
     }
 
-    const response = await fetch(`${CMC_LIST_URL}${CMC_API_KEY}${apiKey}`, {
-      next: { revalidate: 0 },
-    });
+    const response = await fetch(`${CMC_LIST_URL}${CMC_API_KEY}${apiKey}`);
     const data = await response.json();
 
     return new Response(JSON.stringify(data), {
       status: 200,
+      headers: {
+        "Cache-Control": "no-store, max-age=0",
+      },
     });
   } catch (error) {
     return new Response("Server error", {
